a8d097d1c2283b0adce2ce6fa62b8b15f8b17e84
Nathan Yin <ryzr1320@gmail.com>
2016-07-06 17:53:32 -0700

    meow

--- js/movies.js
+++ js/movies.js
@@ -1,1 +1,22 @@
 'use strict';
+
+var myApp = angular.module('MoviesApp', []);
+myApp.controller('MoviesCtrl', ['$scope', '$http', function($scope, $http) {
+    $scope.message = 'Hello World';
+    
+    $http.get('data/movies-2015.json').then(function(response) {
+        var data = response.data;
+        $scope.movies = data;
+    });
+
+    $scope.ordering = '-gross';
+    
+    $scope.sortBy = function(order) {
+        if ($scope.ordering == order) {
+            $scope.ordering = '-'+order;
+        } else {
+            $scope.ordering = order;
+        }
+    };
+
+}]);
